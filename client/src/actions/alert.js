2f9c9829acf093fc49b274762bb98af2373087f4
Jordan Paz <jordandavidpaz@gmail.com>
2019-05-31 08:55:49 -0700

    Add alert reducer and action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,4 +1,4 @@
-import { SET_ALERT, REMVOE_ALERT } from "./types";
+import { SET_ALERT, REMOVE_ALERT } from "./types";
 import uuid from "uuid";
 
 export const setAlert = (msg, alertType) => dispatch => {
